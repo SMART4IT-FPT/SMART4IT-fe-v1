07ec7577c3689fb85b06364b3d123b160a89525b
TruongTrongTien <trongtientruong2003@gmail.com>
2025-04-17 11:30:50 +0700

    chore: fix deployment error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { copyFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-staticwebapp-config',
+      closeBundle() {
+        copyFileSync(
+          resolve(__dirname, 'staticwebapp.config.json'),
+          resolve(__dirname, 'dist/staticwebapp.config.json')
+        );
+      },
+    },
+  ],
 });
