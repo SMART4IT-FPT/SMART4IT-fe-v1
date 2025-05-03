084e2989c51bd961d3e844355aab35af6bbc0975
Trương Trọng Tiến <121301557+TruongTrongTien@users.noreply.github.com>
2025-05-03 10:47:15 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { copyFileSync } from "fs";
+import { copyFileSync, existsSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,12 +10,17 @@ export default defineConfig({
     {
       name: 'copy-staticwebapp-config',
       closeBundle() {
-        copyFileSync(
-          const source = resolve(__dirname, 'staticwebapp.config.json');
-          const target = resolve(__dirname, 'dist/staticwebapp.config.json');
-          console.log('SOURCE PATH:', source);
-          console.log('TARGET PATH:', target);
-        );
+        const source = resolve(__dirname, 'staticwebapp.config.json');
+        const target = resolve(__dirname, 'dist/staticwebapp.config.json');
+
+        console.log('SOURCE PATH:', source);
+        console.log('TARGET PATH:', target);
+
+        if (existsSync(source)) {
+          copyFileSync(source, target);
+        } else {
+          console.warn(`⚠️ Cannot copy staticwebapp.config.json — file not found at: ${source}`);
+        }
       },
     },
   ],
